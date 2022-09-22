66bba9f6598ef916ef6318de65ebd255ba026db0
Steven Gray <steagray@ut.utm.edu>
2022-09-21 22:30:33 -0500

    fixed movement

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -34,16 +34,12 @@ func _process(delta):
 	
 	# Movement handling
 	if Input.is_action_pressed("ui_right") and canMove and not isCasting:
-		rotation_degrees = 90
 		move_and_collide(Vector2.RIGHT * stats.speed)
 	if Input.is_action_pressed("ui_down") and canMove and not isCasting:
-		rotation_degrees = 180
 		move_and_collide(Vector2.DOWN * stats.speed)
 	if Input.is_action_pressed("ui_up") and canMove and not isCasting:
-		rotation_degrees = 0
 		move_and_collide(Vector2.UP * stats.speed)
 	if Input.is_action_pressed("ui_left") and canMove and not isCasting:
-		rotation_degrees = 270
 		move_and_collide(Vector2.LEFT * stats.speed)
 
 func _input(event):
@@ -51,6 +47,14 @@ func _input(event):
 		castSpell(spellone)
 	if event.is_action_pressed("spell_two"):
 		castSpell(spelltwo)
+	if event.is_action_pressed("ui_right"):
+		rotation_degrees = 90
+	if event.is_action_pressed("ui_down"):
+		rotation_degrees = 180
+	if event.is_action_pressed("ui_up"):
+		rotation_degrees = 0
+	if event.is_action_pressed("ui_left"):
+		rotation_degrees = 270
 
 func takedmg():
 	stats.health -= 1
