9dd55d08e3677b3c07079e4849ad7a39a9a7b7e8
Steven Gray <steagray@ut.utm.edu>
2022-09-30 00:49:13 -0500

    overworld has a reset button so no need to reload

--- Scripts/RET2TUT.gd
+++ Scripts/RET2TUT.gd
@@ -1,16 +1,17 @@
-extends Node
+extends Button
 
 
-# Declare member variables here.
-var speed = 10
-var maxHP = 4
-var health = 4  # Health
-var castTimer_MAX = 0.2
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _pressed():
+	get_tree().change_scene("res://Scenes/Tutorial.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
