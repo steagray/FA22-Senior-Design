7122c1e96bf17a3b78b8f5212736ddd9bbaee838
Steven Gray <steagray@ut.utm.edu>
2022-10-05 14:21:59 -0500

    enemy movement progress

--- Scripts/tanky_melee_enemy.gd
+++ Scripts/tanky_melee_enemy.gd
@@ -1,27 +1,32 @@
 extends KinematicBody2D
 
 var enemy_health = 2
-var can_move = true
 var attack_cooldown = 2
+var isAggro
 
 func _ready():
-	pass # Replace with function body.
+	_on_BehaviorTimer_timeout()
 
 # Called when the node is about to leave SceneTree upon freeing or scene changing
 func _exit_tree():
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-# func _process(delta):
+func _input(event):
+	move_and_collide(Vector2.UP)
 
 # Function call for taking damage. Removes sprite from scene when killed
 func takedmg():
 	enemy_health -= 1
 	if enemy_health <= 0:
-		can_move = false
 		queue_free()
 		print("Tango Down")
 
 
 func _on_Button_pressed():
 	takedmg();
+
+
+func _on_BehaviorTimer_timeout():
+	randomize()
+	isAggro = randi() % 2
+	print(isAggro)
