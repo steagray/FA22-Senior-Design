6339e0375064fb94f40bca106cd4ba16925681eb
Steven Gray <steagray@ut.utm.edu>
2022-10-05 19:14:54 -0500

    Enemy movement functioning

--- Scripts/tanky_melee_enemy.gd
+++ Scripts/tanky_melee_enemy.gd
@@ -2,21 +2,24 @@ extends KinematicBody2D
 
 var enemy_health = 2
 var attack_cooldown = 2
+var speed = 500
+var velocity
 var isAggro
-var i = 0
+var player
 
 func _ready():
 	_on_BehaviorTimer_timeout()
+	player = get_tree().root.get_node("Node2D/KinematicPlayer2D")
+	velocity = Vector2.ZERO
 
 # Called when the node is about to leave SceneTree upon freeing or scene changing
 func _exit_tree():
 	pass
 
 func _process(delta):
-	move_and_collide(Vector2.RIGHT)
-
-func _input(event):
-	move_and_collide(Vector2.UP)
+	move_and_slide(velocity)
+	if get_slide_count() != 0:
+		move_and_slide(Vector2(velocity.y, velocity.x))
 
 # Function call for taking damage. Removes sprite from scene when killed
 func takedmg():
@@ -33,4 +36,36 @@ func _on_Button_pressed():
 func _on_BehaviorTimer_timeout():
 	randomize()
 	isAggro = randi() % 2
-	print(isAggro)
+	if isAggro:
+		$MovementTimer.wait_time = 0.25
+	else:
+		$MovementTimer.wait_time = 1
+
+
+func _on_MovementTimer_timeout():
+	var distx = player.position.x - self.position.x # if neg, left of. if pos, right of
+	var disty = player.position.y - self.position.y # if neg, above. if pos, below
+	
+	if isAggro:
+		if abs(distx) > abs(disty) || abs(disty) < 64:
+			velocity = Vector2(speed * (distx / abs(distx)), 0)
+		elif abs(disty) > abs(distx) || abs(distx) < 64:
+			velocity = Vector2(0, speed * disty / abs(disty))
+		else:
+			velocity = Vector2(0, speed * disty / abs(disty))
+		# 2d math
+		pass
+	else:
+		match (randi() % 5):
+			0: 
+				velocity = Vector2.ZERO
+			1:
+				velocity = Vector2.UP * speed
+			2:
+				velocity = Vector2.DOWN * speed
+			3:
+				velocity = Vector2.LEFT * speed
+			4:
+				velocity = Vector2.RIGHT * speed
+			
+		pass
