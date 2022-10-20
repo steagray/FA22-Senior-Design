ffc5742edf6433cf7f5901614ed35d330c09f7a9
jamesnail <jamgnail@ut.utm.edu>
2022-10-20 14:09:41 -0500

    Room 2
    Added room 2 and improved particle effects.

--- Scripts/Dungeon Tileset.gd
+++ Scripts/Dungeon Tileset.gd
@@ -1,22 +1,29 @@
 extends TileMap
 
-# used to store our particles and room locks/plate states when they enter the scene.  DO NOT GET RID OF THIS
+# used to store our particles and room locks/plate states when they enter the scene.
 var BreakDoorParticles
 var MakeDoorParticles
 var ActivatePlateParticles
 var Room1Locked = false
 var Room1Locks = [false, false, false]
+var Room2Plates = [false, false, false]
 
 # for erasing tiles with *style*
 func set_tile(x, y, particles, tileID):
 	set_cell(x, y, tileID)
 	particles.position = map_to_world(Vector2(x, y)) + Vector2(32, 32)
 
+func activate_plate_paricles(x, y):
+	ActivatePlateParticles.emitting = true
+	ActivatePlateParticles.position = map_to_world(Vector2(x,y))
+	yield(get_tree().create_timer(0.25), "timeout")
+	ActivatePlateParticles.emitting = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	BreakDoorParticles = get_tree().get_root().get_children()[3].get_children()[6].get_children()[0]
-	MakeDoorParticles = get_tree().get_root().get_children()[3].get_children()[6].get_children()[1]
-	ActivatePlateParticles = get_tree().get_root().get_children()[3].get_children()[6].get_children()[2]
+	var particles = get_tree().get_root().get_children()[3].get_children()[4].get_children()
+	BreakDoorParticles = particles[0]
+	MakeDoorParticles = particles[1]
+	ActivatePlateParticles = particles[2]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -50,6 +57,7 @@ func LockRoom1(body):
 func OpenRoom1(body):
 	# if we can open the door...
 	if Room1Locks[0] == true and Room1Locks[1] == true and Room1Locks[2] == true and Room1Locked == true:
+		activate_plate_paricles(43, 8)
 		var y = 10 # the y value of the "open shit up" door
 		BreakDoorParticles.emitting = true
 		for i in range(41, 45):
@@ -66,23 +74,64 @@ func OpenRoom1(body):
 func _on_Room1Plate1_body_entered(body):
 	if Room1Locks[0] == false:
 		Room1Locks[0] = true
-		ActivatePlateParticles.emitting = true
-		ActivatePlateParticles.position = map_to_world(Vector2(32, 2))
-		yield(get_tree().create_timer(0.25), "timeout")
-		ActivatePlateParticles.emitting = false
+		activate_plate_paricles(32,2)
 
 func _on_Room1Plate2_body_entered(body):
 	if Room1Locks[1] == false:
 		Room1Locks[1] = true
-		ActivatePlateParticles.emitting = true
-		ActivatePlateParticles.position = map_to_world(Vector2(41, 2))
-		yield(get_tree().create_timer(0.25), "timeout")
-		ActivatePlateParticles.emitting = false
+		activate_plate_paricles(41,2)
 
 func _on_Room1Plate3_body_entered(body):
 	if Room1Locks[2] == false:
 		Room1Locks[2] = true
-		ActivatePlateParticles.emitting = true
-		ActivatePlateParticles.position = map_to_world(Vector2(32, 7))
-		yield(get_tree().create_timer(0.25), "timeout")
-		ActivatePlateParticles.emitting = false
+		activate_plate_paricles(32,7)
+
+func LockRoom2(body):
+	if body.get_instance_id() == get_tree().get_root().get_children()[3].get_children()[3].get_instance_id():
+		var y = 10
+		MakeDoorParticles.emitting = true
+		for i in range(41, 45):
+			set_tile(i, y, MakeDoorParticles, 0)
+			yield(get_tree().create_timer(0.15), "timeout")
+		MakeDoorParticles.emitting = false
+
+func _on_Room2Plate1_body_entered(body):
+	if Room2Plates[0] == false:
+		Room2Plates[0] = true
+		activate_plate_paricles(30,14)
+		BreakDoorParticles.emitting = true
+		var y = 16
+		for x in range(28, 33):
+			set_tile(x, y, BreakDoorParticles, 1)
+			yield(get_tree().create_timer(0.15), "timeout")
+		BreakDoorParticles.emitting = false
+
+func _on_Room2Plate2_body_entered(body):
+	if Room2Plates[1] == false:
+		Room2Plates[1] = true
+		activate_plate_paricles(43, 19)
+		BreakDoorParticles.emitting = true
+		var y = 21
+		for x in range(41, 45):
+			set_tile(x, y, BreakDoorParticles, 1)
+			yield(get_tree().create_timer(0.15), "timeout")
+		BreakDoorParticles.emitting = false
+
+func _on_Room2Plate3_body_entered(body):
+	if Room2Plates[2] == false:
+		activate_plate_paricles(43, 25)
+		var x = 27
+		#23 -27
+		BreakDoorParticles.emitting = true
+		for y in range(23, 27):
+			set_tile(x, y, BreakDoorParticles, 1)
+			yield(get_tree().create_timer(0.15), "timeout")
+		BreakDoorParticles.emitting = false
+		
+		yield(get_tree().create_timer(3), "timeout")
+		
+		MakeDoorParticles.emitting = true
+		for y in range(23, 27):
+			set_tile(x, y, MakeDoorParticles, 0)
+			yield(get_tree().create_timer(0.15), "timeout")
+		MakeDoorParticles.emitting = false
