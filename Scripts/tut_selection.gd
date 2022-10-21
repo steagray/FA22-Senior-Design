69a186a0ea875146d0e15db7373428297cf77fed
Steven Gray <steagray@ut.utm.edu>
2022-10-21 00:44:53 -0500

    podium mechanic framework implemented

--- Scripts/tut_selection.gd
+++ Scripts/tut_selection.gd
@@ -1,16 +1,37 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var optionA
+var optionB
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	setOP("Podium1", get_parent().elemOrder.pop_front())
+	setOP("Podium2", get_parent().elemOrder.pop_front())
 
+func select_option(body):
+	if body.global_position.x <= $Podium1.global_position.x + 200 and body.global_position.x >= $Podium1.global_position.x - 300:
+		# Podium 1 Selected
+		print("Podium 1")
+	elif body.global_position.x <= $Podium2.global_position.x + 200 and body.global_position.x >= $Podium2.global_position.x - 300:
+		# Podium 2 Selected
+		print("Podium 2")
+	
+	# Once something is selected, free the podium so only one can be selected
+	queue_free()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func setOP(option, element):
+	match element:
+		0:
+			# Set fire podium
+			print("Hello")
+		1:
+			# Set water podium
+			print("Hello")
+		2:
+			# Set earth podium
+			print("Hello")
+		3:
+			# Set air podium
+			print("Hello")
+
