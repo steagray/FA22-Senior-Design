d8fbd083e0743dde133d98e6a6d65c93f3c084e2
Steven Gray <steagray@ut.utm.edu>
2022-10-22 17:12:34 -0500

    final cleanup

--- Scripts/tut_selection.gd
+++ Scripts/tut_selection.gd
@@ -10,7 +10,6 @@ func _ready():
 	podiums.append(get_parent().elemOrder.pop_front())
 	
 	# Dynamically set podium sprite textures based on what is generated from tutorial
-	# Sprite will be found in "res://Assets/ElemSprites/[# generated based on elem]"
 	$Podium1/Sprite.region_rect = Rect2(podiums[0] * 64, 0, 64, 64)
 	$Podium2/Sprite.region_rect = Rect2(podiums[1] * 64, 0, 64, 64)
 
@@ -20,11 +19,9 @@ func select_option(body):
 	if body.global_position.x <= $Podium1.global_position.x + 200 and body.global_position.x >= $Podium1.global_position.x - 300:
 		# Podium 1 Selected
 		podium = 0
-		print("Podium 1: " + str(podiums[0]))
 	elif body.global_position.x <= $Podium2.global_position.x + 200 and body.global_position.x >= $Podium2.global_position.x - 300:
 		# Podium 2 Selected
 		podium = 1
-		print("Podium 2: " + str(podiums[1]))
 	
 	if modspell.active:
 		modspell = spelltwo
